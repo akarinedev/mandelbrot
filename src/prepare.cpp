bb0ec6fd0e7fbf2a02172653e1cb9003263d569d
Akari Neukirch <akari@akarine.dev>
2019-10-13 14:51:56 -0400

    More work on prepare, strange errors when custom args are input

--- src/prepare.cpp
+++ src/prepare.cpp
@@ -13,9 +13,6 @@
 #include <string>
 #include <stdio.h>
 
-//My code
-#include "frameinfo.h"
-
 //Libs
 #include "json.hpp"
 
@@ -25,15 +22,14 @@ const int CHARS_PER_PIXEL = 3;
 
 static struct option long_options[] =
 {
-	{"l", required_argument, NULL, 'l'},
-	{"x", required_argument, NULL, 'x'},
-	{"y", required_argument, NULL, 'y'},
-//	{"framerate", required_argument, NULL, 'r'},
-//	{"cx", required_argument, NULL, 'u'},
-//	{"cy", required_argument, NULL, 'v'},
-//	{"zmstart", optional_argument, NULL, 's'},
-//	{"zmend", optional_argument, NULL, 'e'},
-//	{"zmrate", optional_argument, NULL, 'z'},
+	{"l", optional_argument, NULL, 'l'},
+	{"x", optional_argument, NULL, 'x'},
+	{"y", optional_argument, NULL, 'y'},
+	{"framerate", optional_argument, NULL, 'r'},
+	{"cx", optional_argument, NULL, 'u'},
+	{"cy", optional_argument, NULL, 'v'},
+	{"zstart", optional_argument, NULL, 's'},
+	{"zrate", optional_argument, NULL, 'z'}
 };
 
 int main(int argc, char **argv)
@@ -42,11 +38,11 @@ int main(int argc, char **argv)
 
 	int index = 0;
 
-	long length;
-	long sx, sy;
-	int framerate;
-	double cx, cy;
-	double zstart, zend, zrate;
+	long length = 10;
+	long sx = 1920, sy = 1080;
+	int framerate = 60;
+	double cx = 0, cy = -1;
+	double zstart = 2, zrate = 0.3333;
 
 	while((c = getopt_long_only(argc, argv, "", long_options, &index)) != -1)
 	{
@@ -73,9 +69,6 @@ int main(int argc, char **argv)
 		case 's':
 			zstart = std::stod(optarg);
 			break;
-		case 'e':
-			zend = std::stod(optarg);
-			break;
 		case 'z':
 			zrate = std::stod(optarg);
 			break;
@@ -88,10 +81,16 @@ int main(int argc, char **argv)
 
 	nlohmann::json data;
 
+	data["length"] = length;
+	data["framerate"] = framerate;
+
 	for(long i = 0; i < length; i++)
 	{
-		data[i]["resx"] = sx;
-		data[i]["resy"] = sy;
+		data["frames"][i]["resx"] = sx;
+		data["frames"][i]["resy"] = sy;
+		data["frames"][i]["cx"] = cx;
+		data["frames"][i]["cy"] = cy;
+		data["frames"][i]["z"] = zstart / pow(10, i*zrate/framerate);
 	}
 
 	// write prettified JSON to another file
